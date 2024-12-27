7db11a94b9669deb9af8f4e411f1d67da243f4d6
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-12-27 17:51:33 +0530

    Created EventService and EventController

--- InventorySystem/Assets/Scripts/Events/EventService.cs
+++ InventorySystem/Assets/Scripts/Events/EventService.cs
@@ -1,6 +1,27 @@
 
 
-public class EventService
+namespace InventorySystem.Events
 {
-    
+    public class EventService
+    {
+        private static EventService instance;
+        public static EventService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EventService();
+                }
+                return instance;
+            }
+        }
+
+
+
+        public EventService()
+        {
+
+        }
+    }
 }
