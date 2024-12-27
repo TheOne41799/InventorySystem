ff6880d78a1d40eabc8b9e1c42a39be52c57159f
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-12-27 18:34:25 +0530

    PlayerService Basic Setup

--- InventorySystem/Assets/Scripts/Player/PlayerService.cs
+++ InventorySystem/Assets/Scripts/Player/PlayerService.cs
@@ -1,6 +1,27 @@
 
+using UnityEngine;
 
-public class PlayerService
+namespace InventorySystem.Player
 {
-    
+    public class PlayerService
+    {
+        private PlayerView playerView;
+
+
+        public PlayerService(PlayerView playerView) 
+        {
+            this.playerView = playerView;
+            
+        }
+
+        public void Init()
+        {
+            playerView = GameObject.Instantiate(playerView);
+        }
+
+        public int GetAvailableMoney()
+        {
+            return playerView.GetAvailableMoney(); 
+        }
+    }
 }
---
+++ InventorySystem/Assets/Scripts/Player/PlayerView.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace InventorySystem.Player
+{
+    public class PlayerView : MonoBehaviour
+    {
+        private int availableMoney = 0;
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                availableMoney++;
+            }
+            else if(Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                availableMoney--;
+            }
+        }
+
+        public int GetAvailableMoney()
+        {
+            return availableMoney;
+        }
+    }
+}
+
+
