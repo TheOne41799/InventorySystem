f3a89e9469fe7f42d3d102ce9a61292c24c4236c
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-12-27 17:02:41 +0530

    Created Audio Service and its related classes

--- InventorySystem/Assets/Scripts/Audio/AudioService.cs
+++ InventorySystem/Assets/Scripts/Audio/AudioService.cs
@@ -1,6 +1,55 @@
+using System;
+using UnityEngine;
 
-
-public class AudioService
+namespace InventorySystem.Sound
 {
+    public class AudioService
+    {
+        private SoundScriptableObject soundScriptableObject;
+        private AudioSource audioEffects;
+        private AudioSource backgroundMusic;
 
+        public AudioService(SoundScriptableObject soundScriptableObject, AudioSource audioEffectSource, AudioSource bgMusicSource)
+        {
+            this.soundScriptableObject = soundScriptableObject;
+            audioEffects = audioEffectSource;
+            backgroundMusic = bgMusicSource;
+            PlaybackgroundMusic(SoundType.BACKGROUND_MUSIC, true);
+        }
+
+        public void PlaySoundEffects(SoundType soundType, bool loopSound = false)
+        {
+            AudioClip clip = GetSoundClip(soundType);
+
+            if (clip != null)
+            {
+                audioEffects.loop = loopSound;
+                audioEffects.clip = clip;
+                audioEffects.PlayOneShot(clip);
+            }
+            else
+                Debug.LogError("No Audio Clip selected.");
+        }
+
+        private void PlaybackgroundMusic(SoundType soundType, bool loopSound = false)
+        {
+            AudioClip clip = GetSoundClip(soundType);
+
+            if (clip != null)
+            {
+                backgroundMusic.loop = loopSound;
+                backgroundMusic.clip = clip;
+                backgroundMusic.Play();
+            }
+        }
+
+        private AudioClip GetSoundClip(SoundType soundType)
+        {
+            Sounds sound = Array.Find(soundScriptableObject.audioList, item => item.soundType == soundType);
+
+            if (sound.audio != null)
+                return sound.audio;
+            return null;
+        }
+    }
 }
---
+++ InventorySystem/Assets/Scripts/Audio/SoundType.cs
@@ -0,0 +1,14 @@
+
+
+namespace InventorySystem.Sound
+{
+    public enum SoundType
+    {
+        NONE,
+        BACKGROUND_MUSIC,
+        ITEM_PICKED_UP,
+        ITEM_DROPPED,
+        ITEM_PURCHASED,
+        ITEM_SOLD
+    }
+}
